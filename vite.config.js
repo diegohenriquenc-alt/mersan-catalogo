ce40be84ca190debcbbb0fd86875ee9b216af337
diegohenriquenc-alt <diegohenriquenc@gmail.com>
2026-07-16 10:29:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,41 @@ export default defineConfig({
         name: 'Mersan Calçados - Catálogo Loja 261',
         short_name: 'Mersan Catálogo',
         description: 'Catálogo digital da Mersan Calçados - Loja 261',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/icons/icon-512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\/produto\/.*/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'produtos-visitados',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 604800
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    port: 5173
+  }
+})
