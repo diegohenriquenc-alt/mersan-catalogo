5fcbe46a3d06dc10e1d06891f1b594da9fa18835
diegohenriquenc-alt <diegohenriquenc@gmail.com>
2026-07-16 09:53:26 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from 'react-router-dom'
+
+// Casca da aplicação. Mantida propositalmente vazia/leve:
+// o briefing pede interface extremamente limpa, sem telas desnecessárias.
+export default function App() {
+  return <Outlet />
+}
