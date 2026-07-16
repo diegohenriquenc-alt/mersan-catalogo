5625928e2ac97e34028ba25504aadc95092afa25
diegohenriquenc-alt <diegohenriquenc@gmail.com>
2026-07-16 09:52:46 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import './index.css'
+
+// Etapa 4 vai adicionar a rota /produto/:codigo aqui.
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
