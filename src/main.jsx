dfa557edf289b05b4934e78f5b4364d6569aa823
diegohenriquenc-alt <diegohenriquenc@gmail.com>
2026-07-17 00:50:44 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import Home from './pages/Home.jsx'
 import Admin from './pages/Admin.jsx'
 import Produto from './pages/Produto.jsx'
 import Catalogo from './pages/Catalogo.jsx'
@@ -13,10 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
+          <Route index element={<Catalogo />} />
           <Route path="admin" element={<Admin />} />
           <Route path="produto/:codigo" element={<Produto />} />
-          <Route path="catalogo" element={<Catalogo />} />
         </Route>
       </Routes>
     </BrowserRouter>
